79a076fe9817628955597951d7c0baea3e7fe348
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-01 15:38:13 -0400

    epic mode done

--- src/arithmetic.js
+++ src/arithmetic.js
@@ -8,8 +8,26 @@ const divide = (a, b) => a / b
 
 const modulo = (a, b) => a % b
 
-const exponent = function(a, b) {
-  return a ** b
+const exponent = (a, b) => (b === 0 ? 1 : a ** b)
+
+const factorial = function(a) {
+  if (a === 0 || a === 1) {
+    return 1
+  }
+  for (let i = a - 1; i > 1; i--) {
+    a *= i
+  }
+  return a
 }
 
-module.exports = { sum, subtract, multiply, divide, modulo, exponent }
+const perf = function(a) {
+  let sum = 0
+  for (let i = 1; i <= a; i++) {
+    if (a % i == 0) {
+      sum += i
+    }
+  }
+  return sum / 2 === a ? true : false
+}
+
+module.exports = { sum, subtract, multiply, divide, modulo, exponent, factorial, perf }
--- tests/arithmetic.test.js
+++ tests/arithmetic.test.js
@@ -1,4 +1,4 @@
-const { sum, subtract, multiply, divide, modulo, exponent } = require('../src/arithmetic')
+const { sum, subtract, multiply, divide, modulo, exponent, factorial, perf } = require('../src/arithmetic')
 
 describe('Subtract can find the difference', () => {
   test('32 and 14', () => {
@@ -59,7 +59,47 @@ describe('Exponent can multiply the mess out of', () => {
     expect(exponent(2, 3)).toBe(8)
   })
 
+  test('3 and 1', () => {
+    expect(exponent(3, 1)).toBe(3)
+  })
+
+  test('3 and 0', () => {
+    expect(exponent(3, 0)).toBe(1)
+  })
+
   test('negative numbers', () => {
     expect(exponent(-1, 3)).toBe(-1)
+  })
+})
+
+describe('Factorial can multiply to the factor of', () => {
+  test('4', () => {
+    expect(factorial(4)).toBe(24)
+  })
+
+  test('6', () => {
+    expect(factorial(6)).toBe(720)
+  })
+
+  test('5', () => {
+    expect(factorial(5)).toBe(120)
+  })
+})
+
+describe('Perfect numbers are perfect!', () => {
+  test('6', () => {
+    expect(perf(6)).toBe(true)
+  })
+
+  test('28', () => {
+    expect(perf(28)).toBe(true)
+  })
+
+  test('10', () => {
+    expect(perf(10)).toBe(false)
+  })
+
+  test('8128', () => {
+    expect(perf(28)).toBe(true)
   })
 })
