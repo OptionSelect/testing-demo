c500746ab4d796234fda7058f848352211b1af27
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-01 14:32:20 -0400

    adventure mode done

--- src/arithmetic.js
+++ src/arithmetic.js
@@ -2,4 +2,14 @@ const sum = (a, b) => a + b
 
 const subtract = (a, b) => a - b
 
-module.exports = { sum, subtract }
+const multiply = (a, b) => a * b
+
+const divide = (a, b) => a / b
+
+const modulo = (a, b) => a % b
+
+const exponent = function(a, b) {
+  return a ** b
+}
+
+module.exports = { sum, subtract, multiply, divide, modulo, exponent }
--- tests/arithmetic.test.js
+++ tests/arithmetic.test.js
@@ -1,4 +1,4 @@
-const { sum, subtract } = require('../src/arithmetic')
+const { sum, subtract, multiply, divide, modulo, exponent } = require('../src/arithmetic')
 
 describe('Subtract can find the difference', () => {
   test('32 and 14', () => {
@@ -21,5 +21,45 @@ describe('Sum can add', () => {
 
   test('large numbers', () => {
     expect(sum(4096, 2048)).toBe(6144)
+  })
+})
+
+describe('Multiply can multiply', () => {
+  test('4 and 5', () => {
+    expect(multiply(4, 5)).toBe(20)
+  })
+
+  test('large numbers', () => {
+    expect(multiply(1000, 5)).toBe(5000)
+  })
+})
+
+describe('Divide can divide', () => {
+  test('8 and 4', () => {
+    expect(divide(8, 4)).toBe(2)
+  })
+
+  test('large numbers', () => {
+    expect(divide(1000, 4)).toBe(250)
+  })
+})
+
+describe('Modulo can mod', () => {
+  test('8 and 4', () => {
+    expect(modulo(8, 4)).toBe(0)
+  })
+
+  test('weird numbers', () => {
+    expect(modulo(5, 4)).toBe(1)
+  })
+})
+
+describe('Exponent can multiply the mess out of', () => {
+  test('2 and 3', () => {
+    expect(exponent(2, 3)).toBe(8)
+  })
+
+  test('negative numbers', () => {
+    expect(exponent(-1, 3)).toBe(-1)
   })
 })
